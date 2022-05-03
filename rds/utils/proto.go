013a8b2a7546bb667746f808c8b5d25103100bc3
Rohit Agrawal <rohit.agrawal@databricks.com>
2022-05-02 20:18:08 -0400

    [RDS] *New* Add Utility For Proto Parsing (#5)
    [RDS] *New* Add Utility For Proto Parsing

---
+++ rds/utils/proto.go
@@ -0,0 +1,58 @@
+package utils
+
+import (
+	v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	"github.com/pkg/errors"
+	"google.golang.org/protobuf/encoding/protojson"
+	"sigs.k8s.io/yaml"
+)
+
+/**
+ * getJsonBytes convert the yamlBytes to json and return bytes array.
+ */
+func getJsonBytes(yamlBytes []byte) ([]byte, error) {
+	jsonBytes, err := yaml.YAMLToJSON(yamlBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "error occurred while converting JSON -> YAML")
+	}
+	return jsonBytes, nil
+}
+
+// ConvertYamlToRouteConfigurationProto convert yamlBytes to v3 RouteConfiguration proto.
+func ConvertYamlToRouteConfigurationProto(yamlBytes []byte) (*v3.RouteConfiguration, error) {
+	config := &v3.RouteConfiguration{}
+	jsonBytes, err := getJsonBytes(yamlBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "error occurred while creating v3.RouteConfiguration proto")
+	}
+	if err = protojson.Unmarshal(jsonBytes, config); err != nil {
+		return nil, errors.Wrap(err, "error occurred while converting YAML -> v3.RouteConfiguration protobuf")
+	}
+	return config, nil
+}
+
+// ConvertYamlToRouteProto convert yamlBytes to v3 Route proto.
+func ConvertYamlToRouteProto(yamlBytes []byte) (*v3.Route, error) {
+	config := &v3.Route{}
+	jsonBytes, err := getJsonBytes(yamlBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "error occurred while creating v3.Route proto")
+	}
+	if err = protojson.Unmarshal(jsonBytes, config); err != nil {
+		return nil, errors.Wrap(err, "error occurred while converting YAML -> v3.Route protobuf")
+	}
+	return config, nil
+}
+
+// ConvertYamlToVirtualClusterProto convert yamlBytes to v3 VirtualCluster proto.
+func ConvertYamlToVirtualClusterProto(yamlBytes []byte) (*v3.VirtualCluster, error) {
+	config := &v3.VirtualCluster{}
+	jsonBytes, err := getJsonBytes(yamlBytes)
+	if err != nil {
+		return nil, errors.Wrap(err, "error occurred while creating v3.VirtualCluster proto")
+	}
+	if err = protojson.Unmarshal(jsonBytes, config); err != nil {
+		return nil, errors.Wrap(err, "error occurred while converting YAML -> v3.VirtualCluster protobuf")
+	}
+	return config, nil
+}
